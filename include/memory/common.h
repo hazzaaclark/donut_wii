d21dd11665e25dcca0a4900b23cfa53a6314fd7b
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-02-14 22:02:19 +0000

    Add files via upload

---
+++ include/memory/common.h
@@ -0,0 +1,41 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* SDL Reimplementation/Port of The Simpsons - Hit and Run */
+/* Special thanks to the following resources for making this project possible: */
+
+/* libogc: https://github.com/devkitPro/libogc */
+/* sdl-wii: https://github.com/dborth/sdl-wii */
+/* GRRLIB: https://github.com/GRRLIB/GRRLIB */
+/* libwiigui: https://github.com/dborth/libwiigui */
+
+/* THIS FILE IS ABOUT COMMON DATA TYPES USED THROUGHOUT THE PROJECT */
+/* CREATING TYPE DEFINED METHODS TO INSTANTIATE RAW POINTERS FOR MY */
+/* METHODS, FUNCTIONS, ETC */
+
+#ifndef COMMON
+#define COMMON
+
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef UNSIGNED_TYPES
+#define UNSIGNED_TYPES
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+
+#endif 
+
+#ifndef SIGNED_TYPES
+#define SIGNED_TYPES
+
+typedef int8_t S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
+
+#endif 
+
+#endif 
